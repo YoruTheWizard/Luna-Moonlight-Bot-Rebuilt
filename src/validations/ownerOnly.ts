a2d0448fcf153363363f666e447d909358060d35
YoruTheWizard <yoruthewizard@gmail.com>
2024-07-23 20:23:23 -0300

    feat: create "luna says" slash command

---
+++ src/validations/ownerOnly.ts
@@ -0,0 +1,22 @@
+import { ValidationProps } from 'commandkit';
+import { ChatInputCommandInteraction } from 'discord.js';
+import { getMessage } from '../json';
+import { devs } from '../config.json';
+
+const guildOnly = getMessage('guildOnly');
+const ownerOnly = getMessage('ownerOnly');
+
+export default function ({
+  interaction,
+  commandObj,
+}: ValidationProps): boolean {
+  interaction = interaction as ChatInputCommandInteraction;
+  if (!interaction.guildId) {
+    interaction.reply({ content: guildOnly, ephemeral: true });
+    return true;
+  }
+  if (!commandObj.options?.ownerOnly) return false;
+  if (interaction.user.id === devs[0]) return false;
+  interaction.reply({ content: ownerOnly, ephemeral: true });
+  return true;
+}
